b790ea990bc50900391bf078bceb81a299a1c802
diego <diego@dmm888.com>
2014-02-11 11:12:43 +0000

    Publishing

---
+++ IOC_DependencyInjection/NotificationSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IOC.Interfaces;
+
+
+namespace IOC_DependencyInjection
+{
+    public class NotificationSystem
+    {
+        public IMessagingService SVC { get; set; }
+        public NotificationSystem() { } // parametreless constructor in this case property must be filled
+        public NotificationSystem(IMessagingService service)
+        {
+            this.SVC = service;
+        }
+        public void Notify()
+        {
+            if (SVC == null) // check if the object is not null
+            {
+                throw new InvalidOperationException("Please set MessagingService before calling " + "Notify().");
+            }
+
+            SVC.SendMessage();
+        }
+    }
+}
---
+++ IOC_DependencyInjection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IOC.Interfaces;
+using IOC.DeliveryModels;
+
+namespace IOC_DependencyInjection
+{
+    class Program
+    { 
+
+        static void Main(string[] args)
+        {
+            // First case: Constructor Injection
+            IMessagingService imsg = new EmailSender("myname.google.com", "Mark is saying:", "Hello how are you");
+            NotificationSystem ns = new NotificationSystem(imsg);
+            ns.Notify();
+            // Second Case: property injection.
+            ns = new NotificationSystem();
+            imsg = new SMSSender("004479234567", "This is an SMS sent from me");
+            ns.SVC = imsg;
+            Console.WriteLine();
+            ns.Notify();
+            Console.ReadLine();
+        }
+    }
+}
