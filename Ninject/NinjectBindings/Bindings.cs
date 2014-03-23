781ede1960414b9bb20e98736b6a6d191f8cee67
diego <diego@dmm888.com>
2014-03-23 15:56:52 +0000

    Added Ninject implementation in a separate project

---
+++ Ninject/NinjectBindings/Bindings.cs
@@ -0,0 +1,22 @@
+﻿using Ninject.DeliveryModels;
+using Ninject.Interfaces;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninject.NinjectBindings
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IMessagingService>().To<EmailSender>().Named("Email");
+            Bind<IMessagingService>().To<SMSSender>().Named("Sms");
+          
+        }
+    }
+
+}
---
+++ Ninject/Program.cs
@@ -0,0 +1,39 @@
+﻿using Ninject.DeliveryModels;
+using Ninject.Interfaces;
+using Ninject.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+           
+            //First Case we want to resolve Email Sender
+            var p1 = new ConstructorArgument("emailaddress", "diego@dmm888.com");
+            var p2 = new ConstructorArgument("emailsubject", "this is the subject");
+            var p3 = new ConstructorArgument("messagebody", "This is the body of the email");            
+            var processor = kernel.Get<IMessagingService>("Email",p1,p2,p3);
+            NotificationSystem ns = new NotificationSystem(processor);
+            ns.Notify();
+            Console.WriteLine();
+          
+            //Second Case we want to resolve Email Sender
+            var p11 = new ConstructorArgument("telephoneNumber", "07771456918");
+            var p22 = new ConstructorArgument("smsbody", "The Body of the SMS message is this");        
+            var processor1 = kernel.Get<IMessagingService>("Sms", p11, p22);
+            NotificationSystem ns1 = new NotificationSystem(processor1);
+            ns1.Notify();
+            Console.ReadLine();
+
+        }
+    }
+}
